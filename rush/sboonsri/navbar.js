087068e69faec6b51f3adf4dd279e27cd978f874
Yume-ji <twilightyume@gmail.com>
2025-02-07 09:48:05 +0000

    a

---
+++ rush/sboonsri/navbar.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('#menu').click(function() {
+        $('#navbar').width(190);
+    })
+
+    $('#close').click(function() {
+        $('#navbar').width(0);
+    })
+
+})
